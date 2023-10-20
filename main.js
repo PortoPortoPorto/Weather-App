32f5b41a46969f9c4630cec43a5031cd212ddb59
PortoPortoPorto <tristanporterrobertson@gmail.com>
2023-10-20 17:22:15 +1100

    sixth?

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 class Cities {
 
-	constructor(lat, lon) {
+	constructor(name, lat, lon,) {
+		this.name = name; 
 		this.lat = lat;
 		this.lon = lon; 
 	}
 }
-const melb = new Cities(37.81, 144.96); 
-const pert = new Cities(31.9, 115.86);
-const edin = new Cities(55.95, 3.18);
-const norw = new Cities(52.62, 1.29);
-const kunm = new Cities(24.87, 102.83);
-const taic = new Cities(24.14, 120.67); 
+const melb = new Cities('Melbourne', 37.81, 144.96); 
+const pert = new Cities('Perth', 31.9, 115.86);
+const edin = new Cities('Edinburgh', 55.95, 3.18);
+const norw = new Cities('Norwich', 52.62, 1.29);
+const kunm = new Cities('Kunming', 24.87, 102.83);
+const taic = new Cities('Taichung', 24.14, 120.67); 
 
 document.addEventListener("DOMContentLoaded", () => {
 	const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric';
@@ -27,7 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	function loadWeather () {
 		var xhr = new XMLHttpRequest ();
-		console.log(xhr); 
 		xhr.open('GET',`https://api.openweathermap.org/data/2.5/weather?lat=${melb.lat}&lon=${melb.lon}&appid=b4b558caac03a615ae12d402d91ffc58&units=metric`, true); 
 	
 		xhr.onload = function () {
@@ -36,17 +36,23 @@ document.addEventListener("DOMContentLoaded", () => {
 				var weather = JSON.parse(this.responseText); 
 			
 				var tempOutput = `
-					<div class = "max">
-						<h3>Max:</h3><h1>${weather.main.temp_max}</h1>
-					</div>
-					<div class = "min">
-						<h3>Min:</h3><h1>${weather.main.temp_min}</h1>
+					<div class = "Temperature">
+						<h5>Temp:</h5><h1>${weather.main.temp_max}°C</h1>
 					</div>`; 
+
+				var description = ` 
+					<h1>${weather.weather[0].description}</h1>`;
+
+				var icon = `
+					<img src = "http://openweathermap.org/img/w/${weather.weather[0].icon}.png">
+				`;
 				 
 				 console.log(tempOutput); 
-
+				 console.log(description); 
+				document.querySelector('#melb-temp').innerHTML = tempOutput; 
+				document.querySelector('#melb-description-box').innerHTML = description; 
+				document.querySelector('#melb-weather-box').innerHTML = icon;  
 			}
-
 		}; 
 		xhr.send(); 
 	}
